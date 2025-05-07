f7e68af60a7b64f2497461af7d4bdce67beb370a
laurenhquan <laurenhquan@gmail.com>
2025-05-06 22:12:06 -0700

    extra validation for numbered inventory input

--- app_mdb.py
+++ app_mdb.py
@@ -213,13 +213,13 @@ async def create_inventory(
         raise HTTPException(status_code=400, detail="Name must be a string")
     if not isinstance(item.description, str):
         raise HTTPException(status_code=400, detail="Description must be a string")
-    if not isinstance(item.quantity, int) or item.quantity < 1:
+    if not isinstance(item.quantity, int) or isinstance(item.quantity, str) or item.quantity < 1:
         raise HTTPException(status_code=400, detail="Quantity must be a non-negative integer greater than 0")
-    if not isinstance(item.price, float) or not validate_price(item.price) or item.price <= 0:
+    if not isinstance(item.price, float) or isinstance(item.price, str) or not validate_price(item.price) or item.price <= 0:
         raise HTTPException(status_code=400, detail="Price must be a float in a valid US format greater than $0.00")
-    if not isinstance(item.serves, int) or item.serves <= 0:
+    if not isinstance(item.serves, int) or isinstance(item.serves, str) or item.serves <= 0:
         raise HTTPException(status_code=400, detail="Serving size must be greater than 0")
-    if not isinstance(item.calories, int) or item.calories < 0:
+    if not isinstance(item.calories, int) or isinstance(item.calories, str) or item.calories < 0:
         raise HTTPException(status_code=400, detail="Calories cannot be below 0")
     item_doc = item.dict()
     item_doc["owner_id"] = str(user["_id"])
